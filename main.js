6cb56e8eb317bacff7434c3c014979f98cc81c3a
François Daoust <francois@joshfire.com>
2012-07-11 15:19:34 +0200

    First working version of the add-on debugger

--- main.js
+++ main.js
@@ -4,43 +4,86 @@ var addonsEl = document.getElementById('addons');
 var datasource = Joshfire.factory.getDataSource('main');
 var feed = [];
 
+/**
+ * Renders the feed's list and triggers the "list" hook
+ */
 var renderFeed = function () {
+  itemEl.innerHTML = '';
+  addonsEl.innerHTML = '';
   var res = '<ul>';
   for (var i=0; i<feed.length; i++) {
-    res += formatItemInList(feed[i]);
+    res += formatItemInList(feed[i], i);
   }
   res += '</ul>';
   listEl.innerHTML = res;
+
+  addons = Joshfire.factory.getAddOns('list');
+  addons.render(addonsEl, {
+    data: feed,
+    dataEl: listEl.firstChild
+  }, function (err) {
+    console.log('list hook called, error:', err);
+  });
 };
 
-var formatItemInList = function (entry) {
-  var str = '<li>';
+/**
+ * Renders the feed item identified by its index and triggers the "item" hook
+ */
+var renderItem = function (idx) {
+  listEl.innerHTML = '';
+  addonsEl.innerHTML = '';
+  itemEl.innerHTML = formatItem(feed[idx]);
+
+  addons = Joshfire.factory.getAddOns('item');
+  addons.render(addonsEl, {
+    data: feed[idx],
+    dataEl: itemEl
+  }, function (err) {
+    console.log('item hook called, error:', err);
+  });
+};
+
+
+/**
+ * Formats the item for use in a list of items
+ */
+var formatItemInList = function (entry, idx) {
+  return '<li data-idx="' + idx + '">\n' +
+    formatItem(entry) +
+    '</li>\n';
+};
+
+/**
+ * Generates the HTML for the given feed item
+ */
+var formatItem = function (entry) {
+  var str = '';
   var date = null;
   var author = null;
 
   // The title when defined
   if (entry.name) {
-    str += '<h4>' + entry.name + '</h4>';
+    str += '<h4>' + entry.name + '</h4>\n';
   }
 
   // Main image when defined
   if (entry.image && entry.image.contentURL) {
-    str += '<p class="image"><img src="' + entry.image.contentURL + '" alt="" /></p>';
+    str += '<p class="image"><img src="' + entry.image.contentURL + '" alt="" /></p>\n';
   }
   else if (entry.thumbnailUrl) {
-    str += '<p class="image"><img src="' + entry.thumbnailUrl + '" alt="" /></p>';
+    str += '<p class="image"><img src="' + entry.thumbnailUrl + '" alt="" /></p>\n';
   }
   else if (entry.thumbnail && entry.thumbnail[0] && entry.thumbnail[0].contentURL) {
-    str += '<p class="image"><img src="' + entry.thumbnail[0].contentURL + '" alt="" /></p>';
+    str += '<p class="image"><img src="' + entry.thumbnail[0].contentURL + '" alt="" /></p>\n';
   }
   else if ((entry['@type'] === 'ImageObject') && entry.contentURL) {
-    str += '<p class="image"><img src="' + entry.contentURL + '" alt="" /></p>';
+    str += '<p class="image"><img src="' + entry.contentURL + '" alt="" /></p>\n';
   }
 
   // Main author when defined
   author = entry.author ? entry.author[0] : entry.byArtist;
   if (author && author.name) {
-    str += '<p class="author">By ' + author.name + '</p>';
+    str += '<p class="author">By ' + author.name + '</p>\n';
   }
 
   // Main date when defined
@@ -55,33 +98,11 @@ var formatItemInList = function (entry) {
     entry.birthDate ||
     entry.deathDate;
   if (/^[0-9]{4}-[0-9]{2}-[0-9]{2}T/.test(date)) {
-    str += '<p class="date">(' + date.substring(0, 10) + ')</p>';
+    str += '<p class="date">(' + date.substring(0, 10) + ')</p>\n';
   }
 
-  /*
-  if (entry.description) {
-    str += '<p>' + entry.description + '</p>';
-  }
-  */
-
-  str += '</li>';
   return str;
 };
-
-var renderItem = function (item) {
-
-};
-
-function datarefresh() {
-  var idx = selectElement.options[selectElement.selectedIndex].value;
-  var datasource = datasources.children ?
-    datasources.children[idx] :
-    datasources;
-  datasource.find({}, function (err, data) {
-    if (err) datacontent.innerHTML = err;
-    else datacontent.innerHTML = JSON.stringify(data, null, 2);
-  });
-}
 
 // Fetch the datasource
 datasource.find({}, function (err, data) {
@@ -89,22 +110,31 @@ datasource.find({}, function (err, data) {
     listEl.innerHTML = '<pre>' + err + '</pre>';
     return;
   }
+
+  // Save the feed
   feed = data.entries;
+
+  // Render the feed
   renderFeed();
-}
 
-if (datasources.children) {
-  for (var i = 0; i < datasources.children.length; i++) {
-    selectElement.innerHTML += '<option value="' + i + '"' +
-      (i === 0 ? ' selected' : '') + '>' +
-      datasources.children[i].name +
-      '</option>';
-  }
-}
-else {
-  selectElement.innerHTML += '<option value="0">' +
-    datasources.name +
-    '</option>';
-}
+  // Listen to clicks on list items and display an item when
+  // one is clicked
+  listEl.addEventListener('click', function (evt) {
+    var target = evt.target;
+    var idx = 0;
+    while (target) {
+      if (target.nodeName === 'LI') break;
+      target = target.parentNode;
+    }
+    if (target) {
+      idx = parseInt(target.getAttribute('data-idx'), 10);
+      renderItem(idx);
+    }
+  });
 
-datarefresh();
+  // Listen to clicks on an item detail and go back to the list
+  // when the item is clicked
+  itemEl.addEventListener('click', function (evt) {
+    renderFeed();
+  });
+});
